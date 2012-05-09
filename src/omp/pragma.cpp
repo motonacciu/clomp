fa7959fd66557aedc98dd9cf3d03378bdaaea7aa
spellegrini <spellegrini@dps.uibk.ac.at>
2012-05-09 10:51:44 +0200

    GPL -> University of Illinois license change

--- src/omp/pragma.cpp
+++ src/omp/pragma.cpp
@@ -1,20 +1,9 @@
-//*****************************************************************************
-//   This file is part of Clomp.
-//	 Copyright (C) 2012  Simone Pellegrini
+//=============================================================================
+//               	Clomp: A Clang-based OpenMP Frontend
 //
-//   Clomp is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//	 any later version.
-//	 
-//	 Clomp is distributed in the hope that it will be useful,
-//	 but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	 GNU General Public License for more details.
-//
-//	 You should have received a copy of the GNU General Public License
-//	 along with Clomp.  If not, see <http://www.gnu.org/licenses/>.
-//*****************************************************************************
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//=============================================================================
 #include "omp/pragma.h"
 #include "omp/annotation.h"
 
